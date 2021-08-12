6b7f3aba4afabb0990bbd65b276721511194240b
yildirimozew <yildirimozew@hotmail.com>
2021-08-12 23:33:27 +0300

    implemented an user interface

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,73 +1,33 @@
-function computerMove () {
-    let randint = Math.floor(Math.random() * 3);
-    if (randint === 0) {
-        return main("rock");
-    }
-    else if (randint === 1) {
-        return main("paper");
-    }
-    else {
-        return main("scissors");
-    }
+weaponSelection = document.querySelectorAll(".choice");
+winLose = document.querySelector(".win-lose");
+healthAlly = document.querySelector(".health-ally");
+healthEnemy = document.querySelector(".health-enemy");
+weaponSelection.forEach(function(element) {
+    element.addEventListener("click", rps)
+})
+
+function rps() {
+    let num = Math.floor(Math.random() * 3);
+    if (num === 0) {roundResult("win")}
+    else if (num === 1) {roundResult("lose")}
+    else {roundResult("draw")}
 }
 
-function main(computerMove) {
-    let playerMove = window.prompt("Please type in your move").toLowerCase();
-    if (playerMove === computerMove) {
-        return result(playerMove, computerMove, 0)
-    }
-    else {
-        if (playerMove === "rock") {
-            if (computerMove === "paper") {
-                return result(playerMove, computerMove, 2)
-            }
-            else {
-                return result(playerMove, computerMove, 1)
-            }          
-        }
-        else if (playerMove === "paper") {
-            if (computerMove === "scissors") {
-                return result(playerMove, computerMove, 2)
-            }
-            else {
-                return result(playerMove, computerMove, 1)
-    }
-    
-}
-        else {
-            if (computerMove === "rock") {
-                return result(playerMove, computerMove, 2)
-            }
-            else {
-                return result(playerMove, computerMove, 1)
-    }
-}
-    }
-}
-function result(playerMove, computerMove, resultNum) {
-    if (resultNum === 0)  {return "It's a tie!"}
-    else if (resultNum === 1) {playerScore += 1;
-        return playerMove + " beats " + computerMove + ". Player wins!";
-        }
-    else {computerScore += 1;
-        return computerMove + " beats " + playerMove + ". Computer wins!";
-        }
+function roundResult(result) {
+    if (result === "win") {winLose.textContent = "You strike!";
+    healthEnemy.value -= 20;}
+    else if (result === "lose") {winLose.textContent = "You take a hit!";
+    healthAlly.value -= 20;}
+    else {winLose.textContent = "Your weapons clash in midair."}
+
+    if (healthAlly.value === 0) {gameResult("lose")}
+    else if (healthEnemy.value === 0) {gameResult("win")}
 }
 
-function counter() {
-    for (; playerScore < 5 && computerScore < 5;) {
-        alert(computerMove());
-
-    }
-    if (playerScore === 5) {
-        alert("Player wins!")
-    }
-    else {
-        alert("Computer wins!")
-    }
-    
-}
-
-let playerScore = 0;
-let computerScore = 0;
-counter();
+function gameResult(result) {
+    if (result === "win") {winLose.textContent = "You won!";
+    weaponSelection.forEach(function(element) {
+        element.removeEventListener("click", rps)})}
+    else {winLose.textContent = "You lost!";
+    weaponSelection.forEach(function(element) {
+        element.removeEventListener("click", rps)})}}
